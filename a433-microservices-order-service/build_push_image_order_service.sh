0086240717c56c2804a066589068a38533921d28
TegarNH <tegarnaufalh@gmail.com>
2023-06-21 14:52:53 +0700

    add dockerfile, build push image, and link image

---
+++ a433-microservices-order-service/build_push_image_order_service.sh
@@ -0,0 +1,21 @@
+# Tegar Naufal Hanip - tegarnauf_19
+
+# Melakukan build image dari file Dockerfile menjadi image 'order-service'
+echo "======= BUILD IMAGE DOCKER ======="
+docker build -t order-service:v1 .
+
+# Melihat daftar image yang tersedia pada local
+echo "======= DAFTAR IMAGE ======="
+docker image ls
+
+# mengubah nama image sesuai dengan format Github Packages
+echo "======= MENGUBAH NAMA IMAGE GITHUB PACKAGE ======="
+docker tag order-service:v1 ghcr.io/tegarnh/a433-microservices/order-service:v1
+
+# Melakukan login ke github package milik TegarNH menggunakan metode Personal Access Token
+echo "======= LOGIN GITHUB PACKAGE ======="
+echo $GITHUB_PACKAGE_ACCESS_TOKEN | docker login ghcr.io -u TegarNH --password-stdin
+
+# Mengunggah image yang sudah di build sebelumnya ke Github Package milik TegarNH
+echo "======= PUSH IMAGE ======="
+docker push ghcr.io/tegarnh/a433-microservices/order-service:v1
